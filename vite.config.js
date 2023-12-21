687acc4af06f96a43876d5010f9d2c04a6f0b4db
ext.gaomengmeng6 <ext.gaomengmeng6@jd.com>
2023-12-21 12:04:02 +0800

    配置静态资源路径

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'login-vite-vue',
   build: {
     outDir: 'docs',//想要把dist修改成什么名字在这边改
   },
